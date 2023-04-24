89e3916ad9423cf4df60a1c6a2cbc13ecfcd8f83
mohand-zaid <muhanadaleem@gmail.com>
2023-04-24 17:37:19 +0200

    Initial commit: Adding Basic Structure of The Calculator

---
+++ team_calc.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#CORE FUNCTIONS
+###############
+#Standard Calculator
+
+function ADD(){}
+function SUB(){}
+function MUL(){}
+function DIV(){}
+function EXP(){}
+
+###############
+#Programmer Calculator
+
+function BIN(){}
+function DEC(){}
+function OCT(){}
+function HEX(){}
+
+###############
+#Scientific Calculator
+
+function RShift(){}
+function LShift(){}
+function AND(){}
+function OR(){}
+function XOR(){}
+function NOT(){}
+
+
+###############
+#Calculation Functions' Menus
+function standard_calc_menu(){}
+function programmer_calc_menu(){}
+function scientific_calc_menu(){}
+
+#Calculator Menu (just menu)
+function CALC_MENU(){}
+
+#Documentation and Team Members
+function DOC(){}
+
+#Main Menu
+function MAIN(){}
